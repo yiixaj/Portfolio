7da163dc8e6387038208c958c2c74a2d403185d5
yiixaj <romeroandre499@gmail.com>
2025-03-04 20:46:14 -0500

    fourth commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -70,7 +70,7 @@ export default function Navbar() {
           <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
           <span className="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
           <span className="relative text-black group-hover:text-white flex items-center gap-x-3">
-            CV <TbDownload size={16} />
+            Resume <TbDownload size={16} />
           </span>
         </motion.a>
 
